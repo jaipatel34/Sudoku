82b3d889fbd51a541af529e60b45fb523bf2eca5
Lab <jpatel9@ufl.edu>
2023-11-27 17:21:34 -0500

    intro

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ def main():
     pygame.init()
     win = pygame.display.set_mode((WIDTH, WIDTH))
     pygame.display.set_caption('Sudoku')
-    win.fill(background_color)
+    #win.fill((236, 231, 261))
     for i in range(0,10):
         if i%3 == 0:
-            pygame.draw.line(win, (0,0,0), (50 + 50 * i,50), (50 + 50 * i,500), 4)
-            pygame.draw.line(win, (0, 0, 0), (50 , 50 + 50 * i), (500, 50 + 50 * i), 4)
-        pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 2)
-        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
+            pygame.draw.line(win, (236,236,236), (50 + 50 * i,50), (50 + 50 * i,500), 4)
+            pygame.draw.line(win, (236,236,236), (50 , 50 + 50 * i), (500, 50 + 50 * i), 4)
+        pygame.draw.line(win, (236,236,236), (50 + 50 * i, 50), (50 + 50 * i, 500), 2)
+        pygame.draw.line(win, (236,236,236), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
     pygame.display.update()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-main()
+if __name__=="__main__":
+    main()
 
 #JAI
