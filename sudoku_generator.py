46a4c0e7121dc931bf6a06dd313d7147717d67da
alexr <razo.alexander@ufl.edu>
2023-12-04 16:34:34 -0500

    bruh

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -27,10 +27,11 @@ class SudokuGenerator:
 
 
     def valid_in_col(self, col, num):
-        for row in range(self.row_length):
-            if self.board[row][col] == num:
-                return False
-        return True
+
+        if num in self.board[col]:
+            return False
+        else:
+            return True
 
 
     def valid_in_box(self, row_start, col_start, num):
@@ -42,19 +43,19 @@ class SudokuGenerator:
         return True
 
 
-    def is_valid(self, row, col, num):
+    def is_valid(self, board, row, col, num):
         for i in range(9):
-            if self.board[row][i] == num:
+            if board[row][i] == num:
                 return False
 
         for i in range(9):
-            if self.board[i][col] == num:
+            if board[i][col] == num:
                 return False
 
         start_row, start_col = 3 * (row // 3), 3 * (col // 3)
         for i in range(3):
             for j in range(3):
-                if self.board[start_row + i][start_col + j] == num:
+                if board[start_row + i][start_col + j] == num:
                     return False
 
         return True
