0d5a7c50d805e99a69bab411cd5a076e63de6f69
Ahsan Hoq <ahsan.hoq04@gmail.com>
2023-12-04 16:30:08 -0500

    jai

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -43,19 +43,19 @@ class SudokuGenerator:
         return True
 
 
-    def is_valid(self, board, row, col, num):
+    def is_valid(self, row, col, num):
         for i in range(9):
-            if board[row][i] == num:
+            if self.board[row][i] == num:
                 return False
 
         for i in range(9):
-            if board[i][col] == num:
+            if self.board[i][col] == num:
                 return False
 
         start_row, start_col = 3 * (row // 3), 3 * (col // 3)
         for i in range(3):
             for j in range(3):
-                if board[start_row + i][start_col + j] == num:
+                if self.board[start_row + i][start_col + j] == num:
                     return False
 
         return True
